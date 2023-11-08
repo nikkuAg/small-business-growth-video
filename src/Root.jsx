d20a27373203138c643e1b59ead44af22db549d5
Divyansh Agarwal <a.divyansh.25@gmail.com>
2023-11-09 00:24:17 +0530

    Remove unwanted files

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,8 +1,7 @@
 import {Composition} from 'remotion';
-import {HelloWorld} from './HelloWorld';
-import {Logo} from './HelloWorld/Logo';
 import {GrowBuisness} from './GrowBusiness';
 import {Title} from './GrowBusiness/title';
+import {CardContainer} from './GrowBusiness/cardContainer';
 
 // Each <Composition> is an entry in the sidebar!
 
@@ -10,25 +9,9 @@ export const RemotionRoot = () => {
 	return (
 		<>
 			<Composition
-				// You can take the "id" to render a video:
-				// npx remotion render src/index.jsx <id> out/video.mp4
-				id="HelloWorld"
-				component={HelloWorld}
-				durationInFrames={150}
-				fps={30}
-				width={1920}
-				height={1080}
-				// You can override these props for each render:
-				// https://www.remotion.dev/docs/parametrized-rendering
-				defaultProps={{
-					titleText: 'Welcome to Remotion',
-					titleColor: 'black',
-				}}
-			/>
-			<Composition
 				id="GrowBusiness"
 				component={GrowBuisness}
-				durationInFrames={90}
+				durationInFrames={140}
 				fps={30}
 				width={540}
 				height={960}
@@ -44,14 +27,13 @@ export const RemotionRoot = () => {
 					title: 'HOW TO GROW SMALL BUSINESS',
 				}}
 			/>
-			{/* Mount any React component to make it show up in the sidebar and work on it individually! */}
 			<Composition
-				id="OnlyLogo"
-				component={Logo}
-				durationInFrames={150}
+				id="Card"
+				component={CardContainer}
+				durationInFrames={90}
 				fps={30}
-				width={1920}
-				height={1080}
+				width={540}
+				height={960}
 			/>
 		</>
 	);
